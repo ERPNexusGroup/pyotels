289f210b3a75208414ea675148cfd7e372ad58ac
WalterCun <waltercunbustamante@gmail.com>
2026-01-22 00:33:54 -0500

    refactor: Delegate OtelMSScraper logic to OtelsDataServices, apply config defaults, and refine method signatures

--- src/pyotels/scraper.py
+++ src/pyotels/scraper.py
@@ -1,15 +1,14 @@
 # src/pyotels/scarper.py
 from typing import Optional, List, Dict, Union, Any
 
-from pyotels.utils.logger import get_logger
-from . import config
 from pyotels.core.data_processor import OtelsProcessadorData
-from .exceptions import AuthenticationError, NetworkError, ParsingError, DataNotFoundError
 from pyotels.core.models import (
     CalendarReservation, CalendarCategories, ReservationModalDetail, ReservationDetail
 )
+from pyotels.utils.logger import get_logger
+from . import config
+from .exceptions import AuthenticationError, NetworkError, ParsingError, DataNotFoundError
 from .services.data_service import OtelsDataServices
-from .utils.dev import save_html_debug
 
 
 class OtelMSScraper:
@@ -19,22 +18,14 @@ class OtelMSScraper:
     """
 
     def __init__(self, id_hotel: str, username: str, password: str,
-                 use_cache: bool = False,
-                 return_dict: bool = False,
+                 use_cache: Optional[bool] = None,
+                 return_dict: Optional[bool] = None,
                  headless: Optional[bool] = None):
 
         self.logger = get_logger(classname="OtelMSScraper")
-        # self.debug = config.DEBUG
         self.username = username
         self.password = password
         self.id_hotel = id_hotel
-        # self.return_dict = return_dict
-
-        # self.domain = config.BASE_URL or "otelms.com"
-        # self.BASE_URL = f"https://{self.id_hotel}.{self.domain}"
-
-        # Resolver headless: si es None, depende del modo debug (Debug=True -> Headless=False)
-        is_headless = headless if headless is not None else not config.DEBUG
 
         # Inicializar Extractor (Maneja Playwright y Sesión)
         # self.extractor = OtelsExtractor(self.BASE_URL, headless=is_headless, use_cache=use_cache)
@@ -42,9 +33,9 @@ class OtelMSScraper:
             id_hotel=self.id_hotel,
             username=self.username,
             password=self.password,
-            use_cache=use_cache,
-            headless=is_headless,
-            return_dict=return_dict
+            use_cache=use_cache if use_cache is not None else config.USE_CACHE,
+            headless=headless if headless is not None else config.HEADLESS,
+            return_dict=return_dict if return_dict is not None else config.RETURN_DICT
         )
 
     def login(self) -> bool:
@@ -52,10 +43,7 @@ class OtelMSScraper:
         Delega el login al extractor.
         """
         try:
-            return self.service.extractor.login(
-                # username=self.username,
-                # password=self.password
-            )
+            return self.service.extractor.login()
         except AuthenticationError:
             self.logger.error("Fallo en autenticación.")
             raise
@@ -63,32 +51,25 @@ class OtelMSScraper:
             self.logger.error(f"Error inesperado en login: {e}")
             raise NetworkError(f"Error en login: {e}")
 
-    def get_categories(self, target_date_str: str = None, as_dict: Optional[bool] = None) -> Union[
-        CalendarCategories, Dict[str, Any]]:
-        # as_dict = self._resolve_as_dict(as_dict)
+    def get_categories(self, as_dict: Optional[bool] = None) -> Union[CalendarCategories, Dict[str, Any]]:
         try:
-            html_content = self.service.extractor.get_calendar_html(target_date_str)
-            processor = OtelsProcessadorData(html_content)
-            return processor.extract_categories(as_dict=as_dict)
+            return self.service.get_categories_data(as_dict=as_dict)
         except Exception as e:
             if isinstance(e, (NetworkError, AuthenticationError)): raise
             raise ParsingError(f"Error al extraer categorías: {e}")
 
-    def get_reservations(self, target_date_str: str = None, as_dict: Optional[bool] = None) -> Union[
+    def get_reservations(self, start_date: Optional[str] = None, as_dict: Optional[bool] = None,
+                         full_data: Optional[bool] = False) -> Union[
         CalendarReservation, Dict[str, Any]]:
         try:
-            html_content = self.service.extractor.get_calendar_html(target_date_str)
-            save_html_debug(html_content, f"calendar_{target_date_str or 'default'}.html")
-            processor = OtelsProcessadorData(html_content)
-            return processor.extract_reservations(as_dict=as_dict)
+            return self.service.get_reservation_data(as_dict=as_dict, start_date=start_date, full_data=full_data)
         except Exception as e:
             if isinstance(e, (NetworkError, AuthenticationError)): raise
             raise ParsingError(f"Error al extraer grilla: {e}")
 
-    def get_reservations_ids(self) -> List[int]:
+    def get_ids_reservation(self, target_date_str: str = None) -> List[int]:
         try:
-            ids_list = self.service.extractor.get_visible_reservation_ids()
-            return ids_list
+            return self.service.get_ids_reservation(target_date_str)
         except Exception as e:
             if isinstance(e, (NetworkError, AuthenticationError)): raise
             raise ParsingError(f"Error al extraer grilla: {e}")
